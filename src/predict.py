06cfb512d804325cb884c8a638b3f20b3d52f157
unktir <valera47304@gmail.com>
2024-12-23 02:36:54 +0500

    feat(images): add the ability to recognize text from an image

--- src/predict.py
+++ src/predict.py
@@ -1,4 +1,21 @@
+from PIL import Image
+import pytesseract
+
+# Путь к исполняемому файлу Tesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 from utils import *
+
+# Распознавание текста на изображении
+def extract_text_from_image(image_path):
+    try:
+        # Открываем изображение
+        image = Image.open(image_path)
+        # Распознаем текст с изображения
+        text = pytesseract.image_to_string(image, lang='eng')
+        return text
+    except Exception as e:
+        return f"Ошибка: {e}"
 
 # Предсказание
 def predict_message(model, vectorizer, message):
@@ -7,24 +24,21 @@ def predict_message(model, vectorizer, message):
     prediction = model.predict(message_tfidf)
     return 'Spam' if prediction[0] == 1 else 'Ham'
 
-# Предсказание на пользовательских данных
-def predict_messages(messages, model, vectorizer):
-    for message in messages:
-        prediction = predict_message(model, vectorizer, message)
-        print(f"Message: {message}\nPrediction: {prediction}\n")
+# Обработка изображений из папки и создание итогового изображения
+def process_images(folder_path, model, vectorizer):
+    images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.png')]
+    predictions = []
+    
+    # Получение предсказаний для каждого изображения
+    for image_path in images:
+        text = extract_text_from_image(image_path)
+        prediction = predict_message(model, vectorizer, text)
+        print((image_path, prediction))
 
 if __name__ == "__main__":
     # Загрузка модели
     model_version = 'main_model' # изменить при необходимости на "second_model" или на собственную
     model, vectorizer = load_model(f'models/{model_version}/ser_model.pkl', f'models/{model_version}/vectorizer.pkl')
     
-    # Пользовательские сообщения
-    messages = [
-        "Congratulations! You've won a free ticket!",
-        "Reminder: your bill is due tomorrow.",
-        "Win $1000 cash now!",
-        "Let's meet tomorrow at 5pm.",
-    ]
-    
-
-    predict_messages(messages, model, vectorizer)
+    # Пользовательские сообщения в виде изображения
+    process_images("images", model, vectorizer)
